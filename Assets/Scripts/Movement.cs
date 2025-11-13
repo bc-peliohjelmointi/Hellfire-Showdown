a77fd261db7dc9811e780a9dfe51a63b8ee4814a
otpx5a <otpx5a@gmail.com>
2025-11-13 10:41:26 +0200

    bunch animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,36 +12,50 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
     private bool isGrounded;
     private float moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        // Movement input
+        // Get horizontal input
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        // Jump input
+        // Flip sprite depending on direction
+        if (moveInput > 0)
+            sr.flipX = false;
+        else if (moveInput < 0)
+            sr.flipX = true;
+
+        // Jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        // Update animation parameters
+        anim.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
+        anim.SetBool("IsJumping", !isGrounded);
+
     }
 
     void FixedUpdate()
     {
-        // Move the player
+        // Apply horizontal movement
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
         // Ground check
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 
-    // Draw ground check radius in editor
     void OnDrawGizmosSelected()
     {
         if (groundCheck == null) return;
